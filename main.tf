fcdb721c4ee003abdcd5108bd3dd728e2a43b32f
Olivier Hoareau <oha@greenberets.io>
2022-10-09 18:14:16 +0200

    fix: fix

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "api" {
 
 module "lambda" {
   source             = "genstackio/lambda/aws"
-  version            = "0.3.4"
+  version            = "0.3.5"
   file               = var.package_file
   s3_bucket          = var.package_s3_bucket
   s3_key             = var.package_s3_key
