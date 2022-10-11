c0e490944045f748441b2ad12c7bd7bd910d7df9
Olivier Hoareau <oha@greenberets.io>
2022-10-12 01:00:02 +0200

    feat: add dns_alts for cert

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "api" {
   source                     = "genstackio/api-lambda/aws"
-  version                    = "0.6.10"
+  version                    = "0.6.11"
   name                       = var.name
   env                        = var.env
   lambda_arn                 = module.lambda.arn
