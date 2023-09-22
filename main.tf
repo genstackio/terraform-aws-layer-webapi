7c3e547bf29e73195b64af3bf45b688443c9cfdf
Olivier Hoareau <oha@greenberets.io>
2023-09-22 17:36:24 +0200

    fix: fix acls

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "api" {
   source                     = "genstackio/api-lambda/aws"
-  version                    = "0.6.18"
+  version                    = "0.7.0"
   name                       = var.name
   env                        = var.env
   lambda_arn                 = module.lambda.arn
