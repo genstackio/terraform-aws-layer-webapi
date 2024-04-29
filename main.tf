3cbc41663b1b4a516a7a06aa2f9e099c441fddf4
Olivier Hoareau <oha@greenberets.io>
2024-04-29 18:16:51 +0200

    fix: fix

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "api" {
   source                     = "genstackio/api-lambda/aws"
-  version                    = "0.10.3"
+  version                    = "0.10.4"
   name                       = var.name
   env                        = var.env
   lambda_arn                 = module.lambda.arn
