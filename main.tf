f26f4b9c0e1df26a4a54a4e90f06d3661ddbb8a4
Olivier Hoareau <oha@greenberets.io>
2025-08-16 10:26:20 +0200

    fix: fix when no name for function

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "api" {
   source                     = "genstackio/api-lambda/aws"
-  version                    = "0.11.3"
+  version                    = "0.11.4"
   name                       = var.name
   env                        = var.env
   lambda_arn                 = module.lambda.arn
