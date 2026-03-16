34267184751f39a2c79168507da0542e8327e1a6
Olivier Hoareau <240222+ohoareau@users.noreply.github.com>
2026-03-16 10:20:45 +0100

    feat: add extra origins support

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "api" {
   source                     = "genstackio/api-lambda/aws"
-  version                    = "0.11.10"
+  version                    = "0.11.11"
   name                       = var.name
   env                        = var.env
   lambda_arn                 = module.lambda.arn
